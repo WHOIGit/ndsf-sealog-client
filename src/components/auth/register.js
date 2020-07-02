cc49df6bf74cf9f0ad33895f2bd5209988003ecc
Webb Pinner <webbpinner@gmail.com>
2020-07-02 09:17:37 -0400

    recaptcha bug fixes

--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -86,7 +86,6 @@ class Register extends Component {
             sitekey={RECAPTCHA_SITE_KEY}
             theme="dark"
             size="invisible"
-            onChange={this.onCaptchaChange.bind(this)}
           />
           <br/>
         </span>
