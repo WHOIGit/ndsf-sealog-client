af199b29a4c17820ce1ca59ac09342347b628b3b
Webb Pinner <webbpinner@gmail.com>
2021-05-26 12:33:58 -0400

    missing path library

--- src/components/image_preview_modal.js
+++ src/components/image_preview_modal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Path from 'path';
 import { Modal, Image } from 'react-bootstrap';
 import { connectModal } from 'redux-modal';
 import { handleMissingImage } from '../utils';
