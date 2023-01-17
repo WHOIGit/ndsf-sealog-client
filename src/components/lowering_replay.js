22ae8365f8156a219670419b611bd4ed96bfba74
Mathew Schwartzman <mcschwartzman@whoi.edu>
2023-01-17 10:45:38 -0500

    fix: made slider UTC/ISO8601 per issue 33

--- src/components/lowering_replay.js
+++ src/components/lowering_replay.js
@@ -42,6 +42,7 @@ class LoweringReplay extends Component {
       state: null,
       cruise: props.cruise,
       lowering: props.lowering,
+      utcScrubber: true,  // add a toggle in the UI or possibly put this in a user setting
     };
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -121,7 +122,17 @@ class LoweringReplay extends Component {
       let loweringStartTime = moment(this.state.lowering.start_ts);
       let loweringNow = moment(this.props.event.events[v].ts);
       let loweringElapse = loweringNow.diff(loweringStartTime);
-      return moment.duration(loweringElapse).format("d [days] hh:mm:ss");
+
+      if (this.state.utcScrubber){
+        return moment(loweringNow).toISOString(); // formats scrubber tooltip in UTC ISO8601
+      }
+      else{
+        return moment.duration(loweringElapse).format("d [days] hh:mm:ss");
+      }
+
+      
+      
+      
     }
 
     return '';
@@ -312,16 +323,16 @@ class LoweringReplay extends Component {
       return (
         <Card className="border-secondary p-1">
           <div className="d-flex align-items-center justify-content-between">
-              <span className="text-primary">00:00:00</span>
+              <span className="text-primary text-nowrap">{(this.state.utcScrubber ? this.state.lowering.start_ts:'00:00:00')}</span>
               {buttons}
-              <span className="text-primary">{moment.duration(loweringDuration).format("d [days] hh:mm:ss")}</span>
+              <span className="text-primary text-nowrap">{(this.state.utcScrubber ? this.state.lowering.stop_ts:moment.duration(loweringDuration).format("d [days] hh:mm:ss"))}</span>
           </div>
           <div className="d-flex align-items-center justify-content-between">
             <SliderWithTooltip
               className="mx-2"
               value={this.state.replayEventIndex}
               tipFormatter={this.sliderTooltipFormatter}
-              trackStyle={{ opacity: 0.5 }}
+              trackStyle={{ opacity: 0.5 }} 
               railStyle={{ opacity: 0.5 }}
               onBeforeChange={this.handleLoweringReplayPause}
               onChange={this.handleSliderChange}
