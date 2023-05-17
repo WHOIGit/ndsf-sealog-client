9733d1ea629e8e4fd8531b3303da74331f647071
Mathew Schwartzman <mathew.schwartzman@whoi.edu>
2023-05-17 09:50:32 -0400

    fix: added default for coord decimal places (#53)

--- src/components/coordinate.js
+++ src/components/coordinate.js
@@ -9,6 +9,11 @@ export default class Coordinate extends Component {
 
     render() {
 
+        if (!COORD_DECIMALS) {
+            console.log("could not get COORD_DECIMALS from config");
+            var COORD_DECIMALS = 6;
+        }
+
         let num = parseFloat(this.props.data.data_value);
 
         return (num.toFixed(COORD_DECIMALS) + " " + this.props.data.data_uom);
