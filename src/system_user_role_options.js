f8801c946b7965e3cbd15edbf61526efb26e40ff
Nathan Figueroa <nathan.figueroa@whoi.edu>
2025-10-22 16:34:28 -0400

    Guest users no longer supported

--- src/system_user_role_options.js
+++ src/system_user_role_options.js
@@ -1,6 +1,5 @@
 export const systemUserRoleOptions = [
   { value: 'admin', label: 'Admin', description: 'Basically God-like' },
-  { value: 'guest', label: 'Guest', description: 'Login without a password' },
 ];
 
 export const ADMIN = 'admin';
